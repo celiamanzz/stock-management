712c152c70e1a2f5600b2a83f9a48a9b2c2757f6
Sonia Escribano <sescribano@salesforce.com>
2021-03-09 09:21:27 +0100

    init

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const cors = require('cors')
 const {pool} = require('./config')
 const path = require('path');
 
+
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
+app.use(express.static(__dirname + '/public'));
 
 const getStock = (request, response) => {
   pool.query('SELECT * FROM stock', (error, results) => {
